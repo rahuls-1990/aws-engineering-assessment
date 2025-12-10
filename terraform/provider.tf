561d313ed7d320b73ed688bad213dcda957dfb4b
rahul <rshinde161@gmail.com>
2025-12-10 19:23:03 +0200

    fix: erraform credetials issue for security improvemets

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region     = "var.region"
 
   s3_use_path_style           = true
   skip_credentials_validation = true
